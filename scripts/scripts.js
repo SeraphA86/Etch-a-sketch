b0d2776a802291b579931a6c6f2e5cd135c8c9ef
SeraphA86 <SerapA86@gmail.com>
2022-02-28 16:45:44 +0200

    Create events that change the color of the drawing

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,3 +1,18 @@
+function randomizer(min, max)/*Selecting a random number in the range [min, max)*/
+{
+    return Math.floor(Math.random() * (max - min)) + min;
+}
+
+function colorGenerator()
+{
+    let r,g,b, color;
+    r = randomizer(0, 256);
+    g = randomizer(0, 256);
+    b = randomizer(0, 256);
+    color = `rgb(${r}, ${g} ,${b})`;
+    return color;
+}
+
 /* The function uses CSS Grid to create a grid in the selected element.
 The function accepts: 
     -size(the size of the grid (AxA)),
@@ -20,8 +35,13 @@ function gridCreator(size, element, canvas)
 }
 
 
-/*In the document, the <div id="indicator" data-down=X></div> 
-element acts as an indicator of pressing and releasing the mouse button.
+/*In the document <div id="indicator" data-down=X></div>
+, the element acts as an indicator of pressing/releasing the mouse button,
+as well as a color indicator for filling in the grid fields.
+(Since functions have their own scopes,
+to track and respond to press, movements, releasing the mouse button and changing the color palette.
+(For which different functions are responsible),
+a global object was needed with which functions could transfer data to each other).
 X=0 - the button is released.
 X=1 - the button is pressed.
 While the mouse button is pressed and not released,
@@ -32,21 +52,32 @@ the cursor moves when the mouse button is pressed and not released.*/
 function changeColor(color, elements)
 {
     let down = document.querySelector('#indicator');
-
-    down.style.backgroundColor = color;/*Changes the background color of the indicator so that when you pressed,
-                                        you can also paint the grid in the desired color.*/
+    down.style.backgroundColor = color;/*The initial color that will be used to fill in the grid fields.*/
     elements.forEach((button)=>
     {    button.addEventListener('mouseover', ()=>
         {
+ 
             if(down.dataset.down == '1')
             {
-                button.style.backgroundColor = color;
+                if(document.querySelector('#indicator').dataset.randomColor != '1')
+                {
+                    button.style.backgroundColor = document.querySelector('#indicator').style.backgroundColor;
+                }
+                else
+                {
+                    button.style.backgroundColor = colorGenerator();/*With each pass of the mouse, it changes the color of the grid element 
+                                                                      to a completely random RGB value.*/
+                }
             }
         });
     });
 }
 
 const canvas = document.querySelector('.canvas');/*The element in which the grid will be created.*/
+const body = document.querySelector('body');
+const pencil = document.querySelector('.pencil')
+const eraser = document.querySelector('.eraser');
+const rainbow = document.querySelector('.rainbow');
 let gridElement = `<div class='pixel'></div>`;/*A single element of the grid.*/
 
 /*Initial initialization of the grid.*/
@@ -54,7 +85,33 @@ gridCreator(8, gridElement, canvas);
 changeColor('black', document.querySelectorAll('.pixel'));
 
 const gridElementsContainer = document.querySelectorAll('.pixel');/*NodeList containing all grid elements.*/
-const body = document.querySelector('body');
+
+/*In order not to add a lot of repetitive listening to grid elements, events
+that change the color of the drawing change the color of the global indicator,
+which is then referenced by the color-changing function.*/
+
+/*An event that changes the color of the drawing.
+Instructs the color change function to use random colors.*/
+rainbow.addEventListener('click', ()=>
+{
+    document.querySelector('#indicator').dataset.randomColor = '1';
+});
+
+/*An event that changes the color of the drawing.Changes the fill color of the grid element.
+Specifies the color change function not to use random colors.*/
+pencil.addEventListener('click', ()=>
+{
+    document.querySelector('#indicator').dataset.randomColor = '0';
+    document.querySelector('#indicator').style.backgroundColor = 'black';
+});
+/*An event that changes the color of the drawing.
+Changes the fill color of the grid element to the color of the canvas.
+Specifies the color change function not to use random colors.*/
+eraser.addEventListener('click', ()=>
+{
+    document.querySelector('#indicator').dataset.randomColor = '0';
+    document.querySelector('#indicator').style.backgroundColor = 'white';
+});
 
 /*Changing the value of the indicator when the mouse button is released.*/
 body.addEventListener('mouseup', ()=>
